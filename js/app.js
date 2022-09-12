06d79377dce106a0e7fe91fcb88a4fd6210e3e52
Sitpi Rajendran <39497470+SitpiRajendran@users.noreply.github.com>
2022-09-12 16:05:32 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,22 @@
+async function getUsers() {
+    let url = 'https://api.tracker.gg/api/v2/rocket-league/standard/profile/epic/Ascyam_';
+    try {
+        let res = await fetch(url);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function renderUsers() {
+    let users = await getUsers();
+    let html = '';
+    let htmlSegment = `<div class="user">
+                    {users}
+                    </div>`;
+    html += htmlSegment;
+    let container = document.querySelector('.container');
+    container.innerHTML = html;
+}
+
+renderUsers();
